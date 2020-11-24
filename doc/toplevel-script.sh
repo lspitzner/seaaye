7eda2612e9d786e5d50f6e7535ff2651d0e3fc9a
Lennart Spitzner <hexagoxel@hexagoxel.de>
2020-11-24 01:07:19 +0100

    Fix bash variable-forwarding error

--- doc/toplevel-script.sh
+++ doc/toplevel-script.sh
@@ -15,4 +15,4 @@ SEAAYE_VERSION=c183de0b0cceadffd060d8a221f694816910e9c2
 SEAAYE_SOURCE="https://github.com/lspitzner/seaaye/archive/$SEAAYE_VERSION.tar.gz"
 SEAAYE_STORE=$(nix-instantiate --expr "builtins.fetchTarball $SEAAYE_SOURCE" --eval --json | jq -r)
 nix-store -r "$SEAAYE_STORE" --indirect --add-root nix/seaaye >/dev/null
-make -f nix/seaaye/Makefile "$*"
+make -f nix/seaaye/Makefile "$@"
